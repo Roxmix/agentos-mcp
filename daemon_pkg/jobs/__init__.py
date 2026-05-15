7126e22084fd72ae3976110b72b0769804c0674d
Roxmix <boolbay66@gmail.com>
2026-05-15 21:23:55 +0300

    fix: update daemon_pkg jobs imports

---
+++ daemon_pkg/jobs/__init__.py
@@ -0,0 +1,13 @@
+from daemon_pkg.jobs import (
+    memory_decay_job,
+    goal_monitor_job,
+    reflection_analyzer_job,
+    self_maintenance_job,
+)
+
+__all__ = [
+    "memory_decay_job",
+    "goal_monitor_job",
+    "reflection_analyzer_job",
+    "self_maintenance_job",
+]
